bc37173ac93d9d93489b0f2d7df8e36bd514fb62
DN <dilaran@live.com>
2020-02-18 11:24:32 -0800

    changes with no displaying img

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
+app.use(express.static("images")); 
 // Import routes and give the server access to them.
 var routes = require("./controllers/Controller.js");
 
